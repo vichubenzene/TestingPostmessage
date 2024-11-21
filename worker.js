351b0e6edeed559648d6c26e43f97193e493d3cd
BENZENE <84439064+vichubenzene@users.noreply.github.com>
2024-11-21 22:18:07 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,11 @@
 
 // Function to get the current time
 function getCurrentTime() {
-    return new Date().toLocaleTimeString();
-  }
-  
-  // Send the current time every 5 seconds (5000 milliseconds)
-  setInterval(() => {
-    const currentTime = getCurrentTime();
-    postMessage(currentTime); // Send the current time to the main script
-  }, 5000);
-  
+  return new Date().toLocaleTimeString();
+}
+
+// Send the current time every 5 seconds (5000 milliseconds)
+setInterval(() => {
+  const currentTime = getCurrentTime();
+  postMessage(currentTime); // Send the current time to the main script
+}, 5000);
